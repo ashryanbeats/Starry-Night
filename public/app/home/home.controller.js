9703115df5095478e244a1237a09d7f707bdf435
Rachel Lee <lee.rachel.jy@gmail.com>
2015-06-20 18:08:52 -0400

    initiate what a colorful night project

--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -1,4 +1,4 @@
-app.controller('HomeController', function($scope, $http, $firebaseArray, DrawingFactory) {
+app.controller('HomeController', function($scope, $http, $firebaseArray) {
 
   function initiate () { 
     console.log('initiated!');
@@ -8,96 +8,51 @@ app.controller('HomeController', function($scope, $http, $firebaseArray, Drawing
 
   initiate();
 
-  var myPath;
-  var drag = false;
   var tool = new Tool();
-  var drawingRef = new Firebase("https://whereyourdrawinggetslost.firebaseio.com");
-  var drawing = $firebaseArray(drawingRef);
-  var myNewPath;
-  var myArray = [];
-  var drawingArray = [];
-  var currentDrawingIndex = 0;
+  // var drawingRef = new Firebase("https://whereyourdrawinggetslost.firebaseio.com");
+  // var drawing = $firebaseArray(drawingRef);
 
   $scope.msgFromScope = "Try drawing something here!";
 
-  drawing.$loaded().then(function(myDrawings) {
-    
-    tool.onMouseDown = function onMouseDown (event) {
-      drag = true;
-      myPath = new Path();
-      myPath.strokeColor = 'white';
-      myPath.add(event.point);
-      var newPointArr = [event.point];
-      drawing.$add(JSON.stringify(newPointArr)).then(function(drawingRef) {
-        var id = drawingRef.key();
-        currentDrawingIndex = drawing.$indexFor(id);
-        console.log('this is the location in the array', drawing.$indexFor(id))
-      })
-      // currentDrawingIndex = drawingArray.push([event.point]) - 1;
-       // drawingArray[currentDrawingIndex].push(event.point);
-    };
-  
-    tool.onMouseDrag = function onMouseDrag (event) {
-      drag = true;
-      myPath.add(event.point);
+  // var myCircle = new Path.Circle(new Point(100, 70), 50);
 
-      var currentPointArr = JSON.parse(drawing[currentDrawingIndex]);
-      currentPointArr.push(event.point);
-
-      drawing[currentDrawingIndex] = JSON.stringify(currentPointArr);
-      drawing.$save(currentDrawingIndex).then(function(ref) {
-          var id = ref.key();
-          console.log("firebase key", id);
-          console.log("index of this key in our array", drawing.$indexFor(id));
-      });
-
-      console.log('this is drawingArray', drawingArray)
-      // When YOU start drawing
-      // drawing.$add(event.point).then(function(drawingRef) {
-        
-      //   var firebase_id = drawingRef.key();
-        
-      //   console.log("added record with id" + firebase_id);
-      //   console.log('this is the location in the array', drawing.$indexFor(firebase_id));
-      //   myArray.push(firebase_id);
-      // });
-    };
-    console.log('this is myDrawings', myDrawings)
-    // // drawing what is already in firebase
-    // myDrawings.forEach(function(eachDrawing) {
-    //   myPath = new Path();
-    //   myPath.importJSON(eachDrawing.$value);
-
-    //   // keep in track of the initial set of drawings
-    //   myArray.push(eachDrawing.$key);
-    // });
-
-    // console.log('this is myDrawings', myDrawings);
-
-    // // Watching any drawings being drawn by anybody
-    //  myNewPath = new Path();
-    // drawing.$watch(function(event) {
-    //   console.log('this is key', event.key)
-    //   var index = drawing.$indexFor(event.key)
-
-    //   // if the drawing is not in my array, import the new drawing
-    //   if(myArray.indexOf(event.key) === -1) {
-    //     myNewPath.importJSON(drawing[index].$value);
-    //     myArray.push(event.key)
-    //     console.log('this is myArray', myArray)
-    //   }
-    // })
+  var myCircle = new Path.Circle({
+    center: view.center,
+    radius: 30,
+    fillColor: 'red'
   });
+
+  var destination = Point.random() * view.size;
+
+  view.onFrame = function (event) {
+    // Each frame, change the fill color of the path slightly by
+    // adding 1 to its hue:
+    myCircle.fillColor.hue += 1;
+
+    var vector = destination - myCircle.position;
+
+    myCircle.position += vector / 30;
+
+    myCircle.content = Math.round(vector.length);
+
+    if(vector.length < 5) {
+
+      destination = Point.random() * view.size;
+    }
+
+    
+  }
+
+  // tool.onMouseDown = function onMouseDown (event) {
+  //   drag = true;
+  // };
   
-  
+  // tool.onMouseDrag = function onMouseDrag (event) {  
 
+  // };
 
-  tool.onMouseUp = function onMouseUp () {
-    drag = false;
-    // myPath.simplify();
-  };
-
-  // When Firebase initially loaded to controller
-
+  // tool.onMouseUp = function onMouseUp () {
+  //   drag = false;
+  // };
 
 });
